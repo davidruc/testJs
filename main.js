3841cbc2ecc93066a2b59e3251aabea4e1be0e7d
DavidAndrésRuedaChacónCAMPUS <davidarueda18@gmail.com>
2023-03-09 13:39:24 -0500

    feat: :sparkles: Arreglos punto 1
    Se agregaron funciones para determinar cual es la persona que tuvo el mejor promedio

--- main.js
+++ main.js
@@ -1,17 +1,27 @@
 // 1. Construir el algoritmo para un programa que ingrese tres notas de un alumno, si el promedio es menor o igual a 3.9 mostrar un mensaje "Estudie“, de lo contrario un mensaje que diga "becado"
 
+
 let nombres = [];
-let notas = [];
-
+let promedios = [];
 const mensaje = () => {
-    nombres = prompt(`Ingrese el nombre del alumno`)
+    
+    let notas = 0;  
+    let nombre = prompt(`Ingrese el nombre del alumno`);
+    nombres.push(nombre);
     for(let i = 0; i < 3; i++){
-        notas.push = prompt(`Ingrese la nota`);
-
+        notas += parseInt(prompt(`Ingrese la nota`));
+        // console.log(notas);
     };
-
+    let promedio = notas / 3; 
+    promedios.push(promedio);
+    console.log(promedio);
 };
 
-mensaje()
+mensaje();
+mensaje();
 console.log(nombres);
-console.log(notas);
+let posicion = promedios.indexOf(Math.max(...promedios));
+
+
+console.log(`${nombres[posicion]} fue el que tuvo el mayor puntaje con ${promedios[posicion]}`);
+console.log(promedios);
