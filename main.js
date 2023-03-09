18d1a29a328394c1d94be5602d700f706b0ae573
DavidAndrésRuedaChacónCAMPUS <davidarueda18@gmail.com>
2023-03-09 09:32:47 -0500

    feat: :tada: Taller 1 JavaScript
    Inicio primer ejercicio

---
+++ main.js
@@ -0,0 +1,17 @@
+// 1. Construir el algoritmo para un programa que ingrese tres notas de un alumno, si el promedio es menor o igual a 3.9 mostrar un mensaje "Estudie“, de lo contrario un mensaje que diga "becado"
+
+let nombres = [];
+let notas = [];
+
+const mensaje = () => {
+    nombres = prompt(`Ingrese el nombre del alumno`)
+    for(let i = 0; i < 3; i++){
+        notas.push = prompt(`Ingrese la nota`);
+
+    };
+
+};
+
+mensaje()
+console.log(nombres);
+console.log(notas);
