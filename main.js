408e98f9f0ba1a32fccf3fdec8eb8a4a927c2f2f
DavidAndrésRuedaChacónCAMPUS <davidarueda18@gmail.com>
2023-03-09 17:53:40 -0500

    feat: :sparkles: Finalización ejercicio 1
    Se terminó de formular el primer ejercicio para n número de estudiantes.

--- main.js
+++ main.js
@@ -3,25 +3,33 @@
 
 let nombres = [];
 let promedios = [];
+let numeroDeEstudiantes = parseInt(prompt(`Ingrese el número de estudiantes que desea agregar.`));;
 const mensaje = () => {
-    
     let notas = 0;  
     let nombre = prompt(`Ingrese el nombre del alumno`);
     nombres.push(nombre);
     for(let i = 0; i < 3; i++){
         notas += parseInt(prompt(`Ingrese la nota`));
-        // console.log(notas);
     };
     let promedio = notas / 3; 
     promedios.push(promedio);
-    console.log(promedio);
 };
+// Esta función (mensaje()) se encarga de pedir los datos de los nombres de los estudiantes, almarcenar sus notas y calcular el promedio de cada uno
+ 
+for(let y = 0; y < numeroDeEstudiantes; y++){
+    mensaje()
+    if (promedios[y] >= 0 && promedios[y] <= 3.9){
+        console.log(`${nombres[y]} tu promedio fue de ${promedios[y]}. No fue lo suficientemente alto, te recomiendo estudiar`);
+    }
+    else if(promedios[y] > 3.9 && promedios[y] <= 5){
+        console.log(`${nombres[y]} fuiste Becado por tu promedio de ${promedios[y]}, felicitaciones`);
+    } 
+    else{
+        console.log(`Las notas ingresadas del estudiante ${nombres[y]} no están dentro del rango de 0 a 5.`);
+    };
+};
+// En este ciclo for se busca mirar el resultado de cada estudiante y recompensarlo o recomendarle estudiar dependiendo del valor del promedio 
 
-mensaje();
-mensaje();
-console.log(nombres);
 let posicion = promedios.indexOf(Math.max(...promedios));
-
-
-console.log(`${nombres[posicion]} fue el que tuvo el mayor puntaje con ${promedios[posicion]}`);
-console.log(promedios);
+console.log(`${nombres[posicion]} fue el estudiante que tuvo el mayor puntaje con ${promedios[posicion]}`);
+//Esto último no tiene nada que ver con el ejercicio planteado, unicamente busca el valor de promedio máximo del array encuenta la posición y permite saber cuál fue el estudiante con el mejor promedio. 
