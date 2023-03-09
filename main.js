1fad949908015a342e84ef3a1d1ee7a74f6e1db8
DavidAndrésRuedaChacónCAMPUS <davidarueda18@gmail.com>
2023-03-09 18:22:11 -0500

    feat: :rocket: Ejercicio 2 Terminado
    Programa que lee si un número es par o impar y si es mayor de 10

--- main.js
+++ main.js
@@ -1,35 +1,9 @@
-// 1. Construir el algoritmo para un programa que ingrese tres notas de un alumno, si el promedio es menor o igual a 3.9 mostrar un mensaje "Estudie“, de lo contrario un mensaje que diga "becado"
+// 2. Dado un número indicar si es par o impar y si es mayor de 10.
 
-
-let nombres = [];
-let promedios = [];
-let numeroDeEstudiantes = parseInt(prompt(`Ingrese el número de estudiantes que desea agregar.`));;
-const mensaje = () => {
-    let notas = 0;  
-    let nombre = prompt(`Ingrese el nombre del alumno`);
-    nombres.push(nombre);
-    for(let i = 0; i < 3; i++){
-        notas += parseInt(prompt(`Ingrese la nota`));
-    };
-    let promedio = notas / 3; 
-    promedios.push(promedio);
+let numero = (prompt(`Ingrese un número`));
+const parOImpar = () => {
+    if (numero % 2 == 0) console.log(`Su número es par`);
+    else console.log(`Su número es impar`);
+    if (numero > 10) console.log(`y es mayor a 10`);
 };
-// Esta función (mensaje()) se encarga de pedir los datos de los nombres de los estudiantes, almarcenar sus notas y calcular el promedio de cada uno
- 
-for(let y = 0; y < numeroDeEstudiantes; y++){
-    mensaje()
-    if (promedios[y] >= 0 && promedios[y] <= 3.9){
-        console.log(`${nombres[y]} tu promedio fue de ${promedios[y]}. No fue lo suficientemente alto, te recomiendo estudiar`);
-    }
-    else if(promedios[y] > 3.9 && promedios[y] <= 5){
-        console.log(`${nombres[y]} fuiste Becado por tu promedio de ${promedios[y]}, felicitaciones`);
-    } 
-    else{
-        console.log(`Las notas ingresadas del estudiante ${nombres[y]} no están dentro del rango de 0 a 5.`);
-    };
-};
-// En este ciclo for se busca mirar el resultado de cada estudiante y recompensarlo o recomendarle estudiar dependiendo del valor del promedio 
-
-let posicion = promedios.indexOf(Math.max(...promedios));
-console.log(`${nombres[posicion]} fue el estudiante que tuvo el mayor puntaje con ${promedios[posicion]}`);
-//Esto último no tiene nada que ver con el ejercicio planteado, unicamente busca el valor de promedio máximo del array encuenta la posición y permite saber cuál fue el estudiante con el mejor promedio. 
+parOImpar();
