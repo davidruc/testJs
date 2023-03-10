a4d3fff67dcf19fe52a48124a48f8b7ec8b4da14
DavidAndrésRuedaChacónCAMPUS <davidarueda18@gmail.com>
2023-03-10 07:28:31 -0500

    feat: :rocket: Ejercio 6 Terminado
    programa que determina la nota máxima y mínima. Además determina si hay más mujeres que hombres

--- main.js
+++ main.js
@@ -1,10 +1,36 @@
-// 5. Construir el algoritmo que lea por teclado dos números, si el primero es mayor al segundo informar su suma y diferencia, en caso contrario, informar el producto y la división del primero respecto al segundo.
+// 6. Construir el algoritmo en Javascript para un programa para cualquier cantidad de estudiantes que lea el nombre, el sexo y la nota definitiva y halle al estudiante con la mayor nota y al estudiante con la menor nota y cuantos eran hombres y cuantos mujeres.
 
-let num1 = parseFloat(prompt(`Ingrese un número`));
-let num2 = parseFloat(prompt(`Ingrese otro número`));
+let nombres = [];
+let definitivas = [];
+let hombres = 0;
+let mujeres = 0;
+let numeroDeEstudiantes = parseInt(prompt(`Ingrese el número de estudiantes que desea agregar.`));
+for (let i = 0; i < numeroDeEstudiantes; i++){
+    let nombre = prompt(`Ingrese el nombre del alumno`);
+    nombres.unshift(nombre);
+    let promedio = parseFloat(prompt(`Ingrese el promedio del alumno`)); 
+    definitivas.unshift(promedio);
+    let sexo = prompt(`Ingrese el sexo del alumno`); 
+    if (sexo == `m` || sexo == `M`){
+        hombres++;
+    }
+    else if (sexo == `F` || sexo == `f`){
+        mujeres++;
+    }
+    else{
+        console.log(`Ingrese un dato válido.`);
+    };
 
-const operaciones = () =>{
-    if (num1 > num2) return console.log(`Como ${num1} es mayor que ${num2}: Su suma es ${num1 + num2} y la diferencia ${num1 - num2}`);
-    else console.log(`Como ${num1} es menor que ${num2}: El producto es ${num1*num2} y la división es ${num1/num2}`);
+}
+// Esta función (mensaje()) se encarga de pedir los datos de los nombres de los estudiantes, almarcenar sus notas y sexos
+let posicionMax = definitivas.indexOf(Math.max(...definitivas));
+console.log(`${nombres[posicionMax]} fue el estudiante que tuvo el mayor puntaje con ${definitivas[posicionMax]}`);
+let posicionMin = definitivas.indexOf(Math.min(...definitivas));
+console.log(`${nombres[posicionMin]} fue el estudiante que tuvo el mayor puntaje con ${definitivas[posicionMin]}`);
+
+const genero = (mujeres, hombres) => {
+    if (mujeres > hombres) return console.log(`Hay ${mujeres} mujeres y sólo ${hombres} hombres. Por lo que hay más mujeres que hombres `);
+    else if (mujeres < hombres) return console.log(`Hay ${hombres} hombres y sólo ${mujeres} mujeres. Por lo que hay más hombres que mujeres `);
+    else console.log(`Hay la misma cantidad de hombres y mujeres (${mujeres})`);
 };
-operaciones()
+genero(mujeres, hombres);
