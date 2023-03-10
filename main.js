4d9cd6e682eeba63ada09d314a5bf6b42689389d
DavidAndrésRuedaChacónCAMPUS <davidarueda18@gmail.com>
2023-03-10 09:12:02 -0500

    feat: :rocket: Ejercicio 9 Terminado
    programa que permite ingresar el nombre y marca de n atletas, y retorna el nombre de la que tuvo la mejor marca y si supera el record anterior lanza una mensaje

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
-// 8. Programa que Ingrese por teclado:
-/* a. el valor del lado de un cuadrado para mostrar por pantalla el perímetro del mismo
-b. la base y la altura de un rectángulo para mostrar el área del mismo */
+/* 9. N atletas han pasado a finales en salto triple en los juegos olímpicos femenino de 2022. Diseñe un programa que pida por teclado los nombres de cada atleta finalista y a su vez, sus marcas del salto en metros. Informar el nombre de la atleta campeona que se quede con la medalla de oro y si rompió récord, reportar el pago que será de 500 millones. El récord esta en 15,50 metros. */
 
-let lado = parseFloat(prompt(`Ingrese el lado del cuadrado en cm`));
-let perimetro = lado*4;
-console.log(`El perimetro del cuadrado es de ${perimetro} cm`);
+let nombre = [];
+let marcas = [];
 
-let base = parseFloat(prompt(`Ingrese la base del rectángulo en cm`));
-let altura = parseFloat(prompt(`Ingrese la altura del rectángulo en cm`));
-let area = base*altura;
-console.log(`El área del rectángulo es de ${area} cm^2`);
+do{
+    nombre.unshift(prompt(`Ingrese el nombre de la atleta: `));
+    marcas.unshift(parseFloat(prompt(`Ingrese la marca en metros`)));
+}
+while(confirm(`¿Deseas agregar otra deportista?`));
+
+let marcaMax = Math.max(...marcas)
+let posicionMax = marcas.indexOf(marcaMax);
+console.log(`${nombre[posicionMax]} fue la atleta con la mejor marca (Marca: ${marcas[posicionMax]} m).`);
+
+if (marcaMax >= 15.5){
+    console.log(`Además ${nombre[posicionMax]} rompió el anterior record (15.50 metros) y recibirá un pago de 500 millones`);
+};
